48e6cdaea8c58ce0974bae329fc0976062d86563
usuiat <stripewani@gmail.com>
2025-03-09 19:21:21 +0900

    Add error image to AsyncImage.
    This allows preview image to be displayed.

--- ui/src/main/java/net/engawapg/sample/ui/ImageComposable.kt
+++ ui/src/main/java/net/engawapg/sample/ui/ImageComposable.kt
@@ -4,6 +4,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import coil3.compose.AsyncImage
 import net.engawapg.lib.zoomable.rememberZoomState
@@ -16,6 +17,7 @@ fun ImageSample(modifier: Modifier = Modifier) {
         model = "https://github.com/usuiat.png",
         contentDescription = null,
         contentScale = ContentScale.Fit,
+        error = painterResource(R.drawable.noimage),
         onSuccess = { zoomState.setContentSize(it.painter.intrinsicSize) },
         modifier = modifier.zoomable(zoomState)
     )
